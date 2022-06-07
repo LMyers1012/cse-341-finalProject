57a16e1b29ddc8d4839f46a9230745bb6d62f478
Lindsey Myers <candlmyers1012@gmail.com>
2022-06-06 20:58:25 -0600

    remove extras

--- app.js
+++ app.js
@@ -59,11 +59,6 @@ app
   .use(bodyParser.urlencoded({ extended: true }))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
-    // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
